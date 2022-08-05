6d812381fd545bc6f5080cd9e0095cdf3fa3e1d6
MojtabaHejazi <108579727+MojtabaHejazi@users.noreply.github.com>
2022-08-05 22:02:01 +0200

    Add files via upload

---
+++ App_Classification.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
+
+dataset = pd.read_csv('BreastTissue.csv')
+
+x= dataset.iloc[:, :9]
+
+y= dataset.iloc[:, -1]
+
+KNC=KNeighborsClassifier()
+
+KNC.fit(x,y)
+
+pickle.dump(KNC, open('model.pkl','wb'))
+
+model=pickle.load(open('model.pkl','rb'))
+
+
+
